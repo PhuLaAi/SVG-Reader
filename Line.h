5b899ec59ae518d6ded2e5f960f8ed62cad55368
6241Nhan <62422162Nhan@gmail.com>
2025-07-16 20:40:01 +0700

    Update Line.h

--- Line.h
+++ Line.h
@@ -1,31 +1,26 @@
 #ifndef LINE_H
 #define LINE_H
 
-#include <windows.h>
 #include <gdiplus.h>
-#include <string>
-#include "SVGElement.h"
 #include "Point2D.h"
-#include "Color.h"
+#include "Colour.h"
 #include "rapidxml.hpp"
 
 using namespace Gdiplus;
 using namespace rapidxml;
 
-class Line : public SVGElement {
-protected:
+class Line {
+private:
     Point2D start;
     Point2D end;
-    Color stroke;
-    float strokeWidth;
+    Stroke stroke;
 
 public:
     Line();
-    Line(Point2D s, Point2D e, Color color, float width);
-
-    void draw(Graphics& graphics) override;
-    void parse(xml_node<>* node) override;
     ~Line();
+
+    void parse(xml_node<>* node);                // Parse from SVG <line> tag
+    void draw(Graphics& graphics);               // Draw using GDI+
 };
 
 #endif
