8a853685a7137eb3e622088f7276a94e1b9cc8c1
DanielChris-NewDev <hogiaphuca@gmail.com>
2025-07-30 22:08:39 +0700

    Changed Ci

--- Circle.cpp
+++ Circle.cpp
@@ -1,51 +1,11 @@
 #include "Circle.h"
 
 Circle::Circle() {
-	rx = ry = 0;
-	center.setX(0);
-	center.setY(0);
+    rx = ry = 0;
+    center.setX(0);
+    center.setY(0);
 }
 Circle::~Circle() {
-
-}
-
-float Circle::getRX() {
-	return this->rx;
-}
-float Circle::getRY() {
-	return this->ry;
-}
-Point2D Circle::getCenter() {
-	return this->center;
-}
-Colour Circle::getColour() {
-	return this->fill;
-}
-float Circle::getStrokeWidth() {
-    return stroke.getStrokeWidth();
-}
-Colour Circle::getStrokeColour() {
-    return stroke.getStrokeColour();
-}
-
-void Circle::setRX(float x) {
-	rx = x;
-}
-void Circle::setRY(float y) {
-	ry = y;
-}
-void Circle::setCenter(float x, float y) {
-	center.setX(x);
-	center.setY(y);
-}
-void Circle::setColour(Colour color) {
-	fill = color;
-}
-void Circle::setStrokeWidth(float w) {
-    stroke.setStrokeWidth(w);
-}
-void Circle::setStrokeColour(Colour StkColour) {
-    stroke.setStrokeColour(StkColour);
 }
 
 vector<Circle> parseCircle(const string& filename) {
@@ -59,7 +19,7 @@ vector<Circle> parseCircle(const string& filename) {
 
     string xmlContent((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 
-    // Prepare a char buffer
+    // Prepare a char buffer (RapidXML modifies the input)
     vector<char> buffer(xmlContent.begin(), xmlContent.end());
     buffer.push_back('\0');
 
@@ -77,8 +37,7 @@ vector<Circle> parseCircle(const string& filename) {
     // Look for all circles
     for (xml_node<>* node = svg->first_node("circle"); node; node = node->next_sibling("circle")) {
         Circle c;
-	// getting values of each circle's attributes
-	// 1. location and radius
+
         if (xml_attribute<>* cx = node->first_attribute("cx")) {
             c.setCenter(stof(cx->value()), c.getCenter().getY());
         }
@@ -92,11 +51,11 @@ vector<Circle> parseCircle(const string& filename) {
             c.setRX(radius);
             c.setRY(radius);
         }
-	// 2. color fill
-        float fillOpacity = 1.0f;
-        if (xml_attribute<>* fillOp = node->first_attribute("fill-opacity"))
-            fillOpacity = stof(fillOp->value());
 
+        float fillOpacity = 1.0f;
+        if (xml_attribute<>* fillOp = node->first_attribute("fill-opacity")) {
+            fillOpacity = stof(fillOp->value());
+        }
         if (xml_attribute<>* fill = node->first_attribute("fill")) {
             string colorStr = fill->value();
 
@@ -107,6 +66,7 @@ vector<Circle> parseCircle(const string& filename) {
                 c.setColour(Colour(r / 255.0f, g / 255.0f, b / 255.0f, fillOpacity));
             }
             else if (colorStr.substr(0, 4) == "rgb(") {
+                
                 string inner = colorStr.substr(4, colorStr.size() - 5);
                 replace(inner.begin(), inner.end(), ',', ' ');
                 istringstream iss(inner);
@@ -115,7 +75,7 @@ vector<Circle> parseCircle(const string& filename) {
                 c.setColour(Colour(r / 255.0f, g / 255.0f, b / 255.0f, fillOpacity));
             }
         }
-	// 3. stroke color and sizesize
+
         if (xml_attribute<>* strokeWidthAttr = node->first_attribute("stroke-width")) {
             float strokeWidth = stof(strokeWidthAttr->value());
             c.setStrokeWidth(strokeWidth);
@@ -153,11 +113,11 @@ vector<Circle> parseCircle(const string& filename) {
 void drawCircle(Graphics* graphics, vector<Circle>& circles) {
     for (Circle& c : circles) {
         Colour colour = c.getColour();
-	// draws inner circle
+
         SolidBrush brush(Color(
             BYTE(colour.o * 255),  
             BYTE(colour.r * 255),  
-            BYTE(colour.g * 255), 
+            BYTE(colour.g * 255),  
             BYTE(colour.b * 255)   
         ));
 
@@ -170,10 +130,10 @@ void drawCircle(Graphics* graphics, vector<Circle>& circles) {
 
         Colour stroke = c.getStrokeColour();
         float strokeWidth = c.getStrokeWidth();
-	// draws outer circle
+
         if (strokeWidth > 0) {
             Pen pen(Color(
-                BYTE(stroke.o * 255), 
+                BYTE(stroke.o * 255),  
                 BYTE(stroke.r * 255),  
                 BYTE(stroke.g * 255),  
                 BYTE(stroke.b * 255)   
@@ -181,24 +141,4 @@ void drawCircle(Graphics* graphics, vector<Circle>& circles) {
             graphics->DrawEllipse(&pen, x, y, w, h);
         }
     }
-
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-    case WM_PAINT: {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        Graphics graphics(hdc);
-        graphics.Clear(Color(255, 255, 255));
-        vector<Circle>* circles = (vector<Circle>*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-        drawCircle(&graphics, *circles);
-        EndPaint(hwnd, &ps);
-        return 0;
-    }
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
 }
