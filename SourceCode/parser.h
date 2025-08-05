d70b438551b2dc26c5e375d9e33a55f853dee659
aer1al <130777227+aer1al@users.noreply.github.com>
2025-08-05 20:59:58 +0700

    Add parser, polygon, utils, libs, update new rect

---
+++ SourceCode/parser.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "libs.h"
+
+#include "shape.h"
+#include "parser.h"
+#include "rectangle.h"
+#include "ellipse.h"
+#include "circle.h"
+#include "polygon.h"
+#include "polyline.h"
+#include "line.h"
+#include "text.h"
+
+void LoadShapesFromSVG(const char* filename, vector<Shape*>& shapes);
