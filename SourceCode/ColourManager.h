19b9323a07332f68561fdd91cd6f48e1ac8f520a
6241Nhan <62422162Nhan@gmail.com>
2025-08-06 22:16:02 +0700

    Update ColourManager.h
    by removing Colour

--- SourceCode/ColourManager.h
+++ SourceCode/ColourManager.h
@@ -2,16 +2,14 @@
 #define COLOUR_MANAGER_H
 
 #include <unordered_map>
-#include "Colour.h"
-
 
 class ColourManager {
 private:
-    static unordered_map<string, Colour> namedColors;
+    static unordered_map<string, ColourManager> namedColors;
 
 public:
     static void loadNamedColours(const string& filename); 
-    static Colour getColourByName(const string& name, float opacity);
+    static ColourManager getColourByName(const string& name, float opacity);
 };
 
 #endif
