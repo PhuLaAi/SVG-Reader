2ff74102795a42961fd638e6638dc40b6df9fea5
PhuLaAi <phuminhleplus@gmail.com>
2025-08-06 20:09:21 +0700

    Update text.cpp

--- SourceCode/text.cpp
+++ SourceCode/text.cpp
@@ -1,4 +1,4 @@
-﻿#include "text.h"
+#include "text.h"
 
 text::text() {
     fontSize = dx = dy = 0;
@@ -20,10 +20,11 @@ string text::getFontFamily() { return fontFamily; }
 string text::getFontStyle() { return fontStyle; }
 string text::getTextAnchor() { return textAnchor; }
 string& text::getContent() { return content; }
-Colour text::getColour() { return fill; }
-float text::getStrokeWidth() { return stroke.getStrokeWidth(); }
-Colour text::getStrokeColour() { return stroke.getStrokeColour(); }
-
+Color text::getColour() { return fill; }
+float text::getStrokeWidth() { return strokeWidth; }
+Color text::getStrokeColour() { return strokeColor; }
+string text::getFillStr() { return fillStr; }
+string text::getStrokeStr() {return strokeStr;}
 void text::setTextPos(float x, float y) { textPos.setX(x); textPos.setY(y); }
 void text::setDx(float dx) { this->dx = dx; }
 void text::setDy(float dy) { this->dy = dy; }
@@ -32,25 +33,47 @@ void text::setFontFamily(string family) { fontFamily = family; }
 void text::setFontStyle(string style) { fontStyle = style; }
 void text::setTextAnchor(string anchor) { textAnchor = anchor; }
 void text::setContent(string text) { content = text; }
-void text::setColour(Colour colour) { fill = colour; }
-void text::setStrokeWidth(float w) { stroke.setStrokeWidth(w); }
-void text::setStrokeColour(Colour StkColour) { stroke.setStrokeColour(StkColour); }
+void text::setColour(Color colour) { fill = colour; }
+void text::setStrokeWidth(float w) { strokeWidth = w; }
+void text::setStrokeColour(Color StkColour) {strokeColor = StkColour; }
+void text::setFillStr(const std::string& str) { fillStr = str; }
+void text::setStrokeStr(const std::string& str) {strokeStr = str;}
 
 void drawText(Graphics* graphics, vector<text>& texts) {
     for (text& t : texts) {
-        Colour col = t.getColour();
-        SolidBrush brush(Color(BYTE(col.o * 255), BYTE(col.r * 255), BYTE(col.g * 255), BYTE(col.b * 255)));
+        string colourStr = t.getFillStr();
+        float opacity = t.getColour().GetAlpha() / 255.0f;
+        Color color = parseColourString(colourStr, opacity);
+        SolidBrush brush(color);
 
         float x = t.getTextPos().getX() + t.getDx();
         float y = t.getTextPos().getY() + t.getDy();
-
-        FontFamily fontFamily(L"Times");
-        Font font(&fontFamily, t.getFontSize(), FontStyleRegular, UnitPixel);
         PointF point(x, y);
+
+        string familyStr = t.getFontFamily();
+        if (familyStr.empty()) familyStr = "Times New Roman";
+        wstring fontName(familyStr.begin(), familyStr.end());
+        FontFamily fontFamily(fontName.c_str());
+
+        Font font(&fontFamily, t.getFontSize(), FontStyleRegular, UnitPixel);
+
         string& content = t.getContent();
         wstring wcontent(content.begin(), content.end());
-        graphics->DrawString(wcontent.c_str(), -1, &font, point, &brush);
+
+        GraphicsPath path;
+        path.AddString(wcontent.c_str(), -1, &fontFamily, FontStyleRegular, t.getFontSize(), point, nullptr);
+
+        // Fill
+        if (t.getColour().GetA() > 0) {
+            SolidBrush fillBrush(t.getColour());
+            graphics->FillPath(&fillBrush, &path);
+        }
+
+        // Stroke
+        if (t.getStrokeColour().GetA() > 0 && t.getStrokeWidth() > 0) {
+            Pen pen(t.getStrokeColour(), t.getStrokeWidth());
+            pen.SetLineJoin(LineJoinRound);
+            graphics->DrawPath(&pen, &path);
+        }
     }
 }
-
-
