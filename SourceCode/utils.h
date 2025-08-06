50bed4cd566f023360231b1fdf3b3a05907a4f4d
aer1al <130777227+aer1al@users.noreply.github.com>
2025-08-06 15:46:30 +0700

    Update utils.h
    Add prototypes relate to path

--- SourceCode/utils.h
+++ SourceCode/utils.h
@@ -8,3 +8,5 @@ int parseInt(const char* s, int def = 0);
 float parseFloat(const char* s, float def = 0.0f);
 vector<point2D> parsePoints(const string& pointsStr);
 vector<Point> toGdiplusPoints(const vector<point2D>& pts);
+bool isNumber(const string& s);
+vector<string> tokenizePath(const string& d);
