a7e4780e2f2e4f4fc613df9ad8cdb6d1208ca542
aer1al <130777227+aer1al@users.noreply.github.com>
2025-08-06 15:42:57 +0700

    Update libs.h

--- SourceCode/libs.h
+++ SourceCode/libs.h
@@ -6,8 +6,11 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <cctype>
+#include <iostream>
 #include "rapidxml.hpp"
 
 using namespace std;
 using namespace Gdiplus;
+
 using namespace rapidxml;
