5c3d1f0a175da0910a12037c65f305e96be41530
DanielChris-NewDev <hogiaphuca@gmail.com>
2025-08-16 21:45:54 +0700

    Made some style changes to Circle

---
+++ SourceCode/Circle.cpp
@@ -0,0 +1,32 @@
+#include "Lib.h"
+
+circle::circle() : ellipse() {
+	this->setCenter(0, 0);
+	this->setRx(0);
+	this->setRy(0);
+}
+
+circle::~circle() {
+	this->setCenter(0, 0);
+	this->setRx(0);
+	this->setRy(0);
+}
+
+void circle::modifyAttribute() {
+	stringstream ss(line_str);
+	string attribute, val, temp;
+
+	while (ss >> attribute) {
+		getline(ss, temp, '"');
+		getline(ss, val, '"');
+
+		if (attribute == "r") {
+			this->setRx(stof(val));
+			this->setRy(stof(val));
+		}
+		if (attribute == "cx")
+			this->center.setX((stof(val)));
+		if (attribute == "cy")
+			this->center.setY((stof(val)));
+	}
+}
